77bf879f8be0e9181b109814d3b91e0082e8ed58
amaryyl <amaryllis0914@gmail.com>
2017-06-24 16:54:16 -0400

    Update route1.asm

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,16 +1,12 @@
 Route1Mons:
 ; Grass Mons
 	db $19
-	db 3,PIDGEY
-	db 3,RATTATA
-	db 3,RATTATA
-	db 2,RATTATA
-	db 2,PIDGEY
-	db 3,PIDGEY
-	db 3,PIDGEY
-	db 4,RATTATA
-	db 4,PIDGEY
-	db 5,RATTATA
+	db 3,EEVEE
+	db 3,EEVEE
+	db 3,VULPIX
+	db 2,ODDISH
+	db 2,HORSEA
+	db 3,RHYDON
 
 ; Water Mons
 	db $00
